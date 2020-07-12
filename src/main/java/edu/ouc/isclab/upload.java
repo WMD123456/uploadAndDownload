6caf48f93e0fbad8fa08834a0dbdf073da7765ca
wmd <1282753259@qq.com>
2020-07-12 20:43:38 +0800

    完成manager第一次上传

---
+++ src/main/java/edu/ouc/isclab/upload.java
@@ -0,0 +1,8 @@
+package edu.ouc.isclab;
+
+public class upload {
+    public static void main(String[] args) {
+        System.out.println("我是wmd123456户主。！！！");
+        System.out.println("wmd123456写的这些内容！！！。");
+    }
+}
