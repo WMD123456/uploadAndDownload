b5f432fd8ff13f5a675589875e18120c4667b65d
wmd <1282753259@qq.com>
2020-07-12 20:45:12 +0800

    manager第一次修改

--- src/main/java/edu/ouc/isclab/upload.java
+++ src/main/java/edu/ouc/isclab/upload.java
@@ -4,5 +4,9 @@ public class upload {
     public static void main(String[] args) {
         System.out.println("我是wmd123456户主。！！！");
         System.out.println("wmd123456写的这些内容！！！。");
+
+        System.out.println("wmd123456：第一次修改，这是我第一次修改的内容。");
+
+
     }
 }
